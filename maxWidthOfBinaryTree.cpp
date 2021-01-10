ddf2a17da499d20312afc3f1d48cd302d0c7a3b7
Abhishek Dilip Dhok <53620210+abhishek143-tech@users.noreply.github.com>
2021-01-10 03:57:35 -0500

    Add files via upload

---
+++ maxWidthOfBinaryTree.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+struct node *createNode(int data)
+{
+    struct node *n;                                 // creating a node pointer
+    n = (struct node *)malloc(sizeof(struct node)); // Allocating memory in the heap
+    n->data = data;                                 // Setting the data
+    n->left = NULL;                                 // Setting the left and right children to NULL
+    n->right = NULL;                                // Setting the left and right children to NULL
+    return n;                                       // Finally returning the created node
+}
+
+int maxWidth(struct node *root)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+    queue<struct node *> q;
+    q.push(root);
+    int res = 0;
+    while (q.empty() == false)
+    {
+        int count = q.size();
+        res = max(res, count);
+        for (int i = 0; i < res; i++)
+        {
+            struct node *curr = q.front();
+            q.pop();
+            if (curr->left != NULL)
+            {
+                q.push(curr->left);
+            }
+            if (curr->right != NULL)
+            {
+                q.push(curr->right);
+            }
+        }
+    }
+    return res;
+}
+
+int main()
+{
+
+    // Constructing the root node - Using Function (Recommended)
+    struct node *root = createNode(9);
+    struct node *p1 = createNode(3);
+    struct node *p2 = createNode(6);
+    struct node *p3 = createNode(1);
+    struct node *p4 = createNode(2);
+    struct node *p5 = createNode(4);
+    // Finally The tree looks like this:
+    //      4
+    //     / \
+    //    1   6
+    //   / \
+    //  5   2
+
+    // Linking the root node with left and right children
+    root->left = p1;
+    root->right = p2;
+    p1->left = p3;
+    p1->right = p4;
+    p2->left = p5;
+   int maxw = maxWidth(root);
+   cout<<maxw<<endl;
+
+    return 0;
+}
