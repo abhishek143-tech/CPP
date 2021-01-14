b139d4d78b73b1d53732ad58fef14fdaf48a20dd
Abhishek Dilip Dhok <53620210+abhishek143-tech@users.noreply.github.com>
2021-01-14 10:53:37 -0500

    Add files via upload

---
+++ ceiling_on_the_left_side.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+void printCeiling(int arr[],int n){
+    cout<<"-1"<<" ";
+    set<int > s;
+    s.insert(arr[0]);
+    for (int i = 1; i < n; i++)
+    {
+        if(s.lower_bound(arr[i])!=s.end()){
+            cout<<*(s.lower_bound(arr[i]))<<" ";
+        }
+        else{
+            cout<<"-1"<<" ";
+        }
+        s.insert(arr[i]);
+    }
+
+    
+}
+int main(){
+     ios::sync_with_stdio(0);
+     cin.tie(0);
+     int arr[]={10,20,30,40};
+     int n = sizeof(arr)/sizeof(arr[0]);
+     printCeiling(arr,n);
+     return 0;
+}
