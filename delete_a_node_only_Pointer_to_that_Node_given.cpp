2e3bd19078dcc8c251f9e8c174bb11b5792d069b
Abhishek Dilip Dhok <53620210+abhishek143-tech@users.noreply.github.com>
2021-02-08 06:25:11 -0500

    Add files via upload

---
+++ delete_a_node_only_Pointer_to_that_Node_given.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *prev;
+    Node *next;
+    Node(int d)
+    {
+        data = d;
+        prev = NULL;
+    }
+};
+
+void printlist(Node *head)
+{
+    Node *curr = head;
+    while (curr != NULL)
+    {
+        cout << curr->data << " ";
+        curr = curr->next;
+    }
+    cout << endl;
+}
+
+void deleteNode(Node *given) // interview question we just have to one pointer which is pointing to the next node of given node and delete that node .
+{
+    Node *temp = given->next;
+    given->data = temp->data;
+    given->next = temp->next;
+    delete(temp);
+
+}
+int main()
+{
+    Node *head = new Node(10);
+    Node *temp1 = new Node(20);
+    Node *temp2 = new Node(30);
+    head->next = temp1;
+    temp1->next = temp2;
+    deleteNode(temp1);
+    printlist(head);
+    return 0;
+}
